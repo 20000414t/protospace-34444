e5619ba8c6923da2872c324ae10c78ab99fbaffe
20000414t <78241361+20000414t@users.noreply.github.com>
2021-02-16 17:53:32 +0900

    新規投稿導入
    buTロウグアウトでルーティングエラーが出てしまった

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,14 @@ class PrototypesController < ApplicationController
 
   def index
   end
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+  def create
+  end
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,3 @@
 class UserController < ApplicationController
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :name, presence: true
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
+  
 end
