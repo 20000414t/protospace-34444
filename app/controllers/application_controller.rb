434f21dda5f93a046028ce5eb7c96135118acdbe
20000414t <78241361+20000414t@users.noreply.github.com>
2021-02-16 12:35:33 +0900

    ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,2 @@
 class ApplicationController < ActionController::Base
-  def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    User.create
-  end
-
-  private
-  def user_params
-    params.require(:user).parmit(:name, :text)
-  end
-  
 end
