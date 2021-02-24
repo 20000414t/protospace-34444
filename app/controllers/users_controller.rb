3bcf2f5348695fdae760a4ea08b5664a315e90aa
20000414t <78241361+20000414t@users.noreply.github.com>
2021-02-24 11:52:32 +0900

    修正の完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
  
   def show
-    #@name = current_user.name
-    #@prototype = current_user.prototypes
+    @name = current_user.name
+    @prototype = current_user.prototypes
     user = User.find(params[:id])
     @prototypes = user.prototypes
     @namae = user.name
